9eab30b0a4751fb294170c57fe08b8ab6abc717b
Andrei Vorotynskii <andrei.vorotynskii@ssh.com>
2022-06-04 21:27:21 +0300

    Adds test.js to check playwright installation

---
+++ src/tests/js/src/tests/test.js
@@ -0,0 +1,14 @@
+const playwright = require('playwright')
+
+(async() => {
+        
+        const browser = await playwright["chromium"].launch({
+		headless: false
+	});
+        
+        const context = await browser.newContext();
+        
+        const page = await context.newPage();
+        
+        await page.goto("https://www.aredel.com/qa/ui/basic/");
+})
