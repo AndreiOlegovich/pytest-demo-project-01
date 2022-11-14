fc8699be5883d3b510a0ce1e2c26e9a832d4b562
Andrei Vorotynskii <andrei.vorotynskii@ssh.com>
2022-11-14 12:35:03 +0200

    QA: fixes param_prod test

--- src/tests/pytest/src/tests/flask-service/test_general.py
+++ src/tests/pytest/src/tests/flask-service/test_general.py
@@ -28,20 +28,33 @@ def test_get_mult_info():
     "args, expected_result",
     [
         pytest.param((0, 0), 0, id="zero - zero"),
-        # pytest.param((0, 1), 0, id="zero - positive"),
-        # pytest.param((0, -2), 0, id="zero - negative"),
-        # pytest.param((3, 0), 0, id="positive - zero"),
-        # pytest.param((-4, 0), 0, id="negative - zero"),
-        # pytest.param((0.0, 0), 0, id="float zero - zero"),
-        # pytest.param((0, 0.0), 0, id="zero - float zero"),
-        # pytest.param((0.0, 0.0), 0, id="float zero - float zero"),
-        # pytest.param((5, 6), 30, id="positive - positive"),
-        # pytest.param((7, -8), -56, id="positive - negative"),
-        # pytest.param((-9, 10), -90, id="negative - positive"),
-        # pytest.param((-11, -12), 132, id="negative - negative"),
-        # pytest.param((13.0, 14), 182, id="float positive - positive"),
+        pytest.param((0, 1), 0, id="zero - positive"),
+        pytest.param((0, -2), 0, id="zero - negative"),
+        pytest.param((3, 0), 0, id="positive - zero"),
+        pytest.param((-4, 0), 0, id="negative - zero"),
+        pytest.param((0.0, 0), 0, id="float zero - zero"),
+        pytest.param((0, 0.0), 0, id="zero - float zero"),
+        pytest.param((0.0, 0.0), 0, id="float zero - float zero"),
+        pytest.param((5, 6), 30, id="positive - positive"),
+        pytest.param((7, -8), -56, id="positive - negative"),
+        pytest.param((-9, 10), -90, id="negative - positive"),
+        pytest.param((-11, -12), 132, id="negative - negative"),
+        pytest.param((13.0, 14), 182, id="float positive - positive"),
     ])
 def test_param_prod(args, expected_result):
-    payload = {"factor_1": args[0], "factor_2": args[1]}
-    res = requests.post(f"http://{target_ip}:5000/mult", data=payload)
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "factor_1": args[0],
+        "factor_2": args[1]
+    }
+    payload = json.dumps(payload)
+    print("payload: ", payload)
+    url = f"http://{target_ip}:5000/mult"
+    res = requests.post(url, headers=headers, data=payload)
+    res = res.json()["Success"]
+
+
     assert res == expected_result
